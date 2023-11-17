c949022d11a292b496c2806ce700bdc1425324a1
Anitha <anitha2002priya2003@gmail.com>
2023-11-17 23:26:42 +0530

    added files

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/userManagement', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // State for the account creation form
+  const [newUser, setNewUser] = useState({
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    location: '',
+    contactDetails: '',
+  });
+
+  useEffect(() => {
+    // Fetch users from the backend
+    axios.get('http://localhost:5000/api/users')
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
+
+  const handleAccountCreation = (event) => {
+    event.preventDefault();
+    // Send the new user data to the backend for creation
+    axios.post('http://localhost:5000/api/users', newUser)
+      .then((response) => {
+        // Handle success (you may want to update the user list or show a success message)
+        console.log('User created successfully:', response.data);
+      })
+      .catch((error) => {
+        console.error('Error creating user:', error);
+      });
+  };
+
+  const filteredUsers = users.filter((user) =>
+    user.username.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User Management Dashboard</h1>
+
+      {/* User Details Tab */}
+      <div>
+        <h2>User Details</h2>
+        <input
+          type="text"
+          placeholder="Search by username"
+          onChange={handleSearch}
+        />
+        <table>
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Password</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredUsers.map((user) => (
+              <tr key={user._id}>
+                <td>{user.username}</td>
+                <td>{user.password}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {/* Account Creation Tab */}
+      <div>
+        <h2>Account Creation</h2>
+        <form onSubmit={handleAccountCreation}>
+          <label>Username:
+            <input
+              type="text"
+              name="username"
+              value={newUser.username}
+              onChange={handleInputChange}
+            />
+          </label>
+
+          <label>Password:
+            <input
+              type="password"
+              name="password"
+              value={newUser.password}
+              onChange={handleInputChange}
+            />
+          </label>
+
+          <label>First Name:
+            <input
+              type="text"
+              name="firstName"
+              value={newUser.firstName}
+              onChange={handleInputChange}
+            />
+          </label>
+
+          <label>Last Name:
+            <input
+              type="text"
+              name="lastName"
+              value={newUser.lastName}
+              onChange={handleInputChange}
+            />
+          </label>
+
+          <label>Location:
+            <input
+              type="text"
+              name="location"
+              value={newUser.location}
+              onChange={handleInputChange}
+            />
+          </label>
+
+          <label>Contact Details:
+            <input
+              type="text"
+              name="contactDetails"
+              value={newUser.contactDetails}
+              onChange={handleInputChange}
+            />
+          </label>
+
+          <button type="submit">Create Account</button>
+        </form>
+      </div>
     </div>
   );
 }
